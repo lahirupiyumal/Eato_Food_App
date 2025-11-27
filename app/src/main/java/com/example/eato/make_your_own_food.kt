20091e90a1e96151389799ff6dfe1b38d1c73f33
lahirupiyumal <166727972+lahirupiyumal@users.noreply.github.com>
2025-11-27 17:14:56 +0000

    first commit my eato app

---
+++ app/src/main/java/com/example/eato/make_your_own_food.kt
@@ -0,0 +1,20 @@
+package com.example.eato
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class make_your_own_food : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_make_your_own_food)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
